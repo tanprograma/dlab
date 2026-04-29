6d1de8f1858b8e810e1312d7f70d7e751220f99a
kija mnada <tanprograma@gmail.com>
2026-04-29 14:35:44 +0300

    fixed routes in [serve.routes.config]

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'form',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'summary',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender,
   },
